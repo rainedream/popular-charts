fdcff591525eee85e90850107cbaae8e48dfe298
MENG Yu <raineddream@gmail.com>
2015-06-08 14:43:29 +0800

    Render realtime data in HighCharts

--- performance/js/highcharts.js
+++ performance/js/highcharts.js
@@ -40,6 +40,63 @@
 	    });
 	};
 
+	highcharts.renderLineWithRealtimeData = function(divId, initData, appendRealtimeData) {
+		Highcharts.setOptions({
+            global: {
+                useUTC: false
+            }
+        });
+
+        $('#' + divId).highcharts({
+            chart: {
+                type: 'spline',
+                animation: Highcharts.svg, // don't animate in old IE
+                marginRight: 10,
+                events: {
+                    load: function () {
+                        // set up the updating of the chart each second
+                        var series = this.series[0];
+                        appendRealtimeData(series);
+                    }
+                }
+            },
+            title: {
+                text: 'Shanghai Composite Index'
+            },
+            xAxis: {
+                type: 'datetime',
+                tickPixelInterval: 150
+            },
+            yAxis: {
+                title: {
+                    text: 'Value'
+                },
+                plotLines: [{
+                    value: 0,
+                    width: 1,
+                    color: '#808080'
+                }]
+            },
+            tooltip: {
+                formatter: function () {
+                    return '<b>' + this.series.name + '</b><br/>' +
+                        Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
+                        Highcharts.numberFormat(this.y, 2);
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            exporting: {
+                enabled: false
+            },
+            series: [{
+                name: 'SHCOMP',
+                data: initData
+            }]
+        });
+	};
+
 	function extractPartialData(dataSeries, key) {
 		var array = new Array();
 		for (var i = 0; i < dataSeries.length; i++) {
