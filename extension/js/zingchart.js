cff690883bb3ace0bc10238aed7a9acc4cbd4283
MENG Yu <raineddream@gmail.com>
2015-06-25 10:58:38 +0800

    Natively zoom in/out data in range

--- extension/js/zingchart.js
+++ extension/js/zingchart.js
@@ -303,4 +303,77 @@
 		renderLine(divId, dataSeries);
 		renderScopeControl(divId, dataSeries);
 	};
+
+	zingchart.renderLineWithinScopeInNativeMode = function(divId, xAxis, dataSeries) {
+		var chartData = {
+		    "background-color":"none",
+		    "type":"line",
+		    "title":{
+		        "text":"Monthly Average Temperature",
+		        "color":"#333",
+		        "background-color":"white"
+		    },
+		    "subtitle":{
+		        "text":"Source: WorldClimate.com"
+		    },
+			"legend":{
+		        "layout":"x1",
+		        "width":"5%",
+		        "margin-top":47,
+		        "border-width":"0",
+		        "shadow":false,
+		        "marker":{
+		            "cursor":"hand",
+		            "border-width":"0"
+		        },
+		        "background-color":"white",
+		        "item":{
+		            "cursor":"hand"
+		        },
+		        "toggle-action":"remove"
+		    },
+		    "scale-x":{
+			    "zooming":true,
+			    "values":xAxis,
+			    "zoom-to-values":["2000/1", "2010/12"]
+			},
+			"scale-y":{
+		        "line-color":"#333"
+			},
+			"preview":{
+			},
+            "tooltip": {
+                "visible": false
+            },
+			"plot":{
+		        "line-width":3,
+		        "marker":{
+		            "size":2
+		        },
+		        "selection-mode":"multiple",
+		        "background-mode":"graph",
+		        "selected-state":{
+		            "line-width":4
+		        },
+		        "background-state":{
+		            "line-color":"#eee",
+		            "marker":{
+		                "background-color":"none"
+		            }
+		        }
+			},
+		    "plotarea":{
+		        "width":"95%",
+		        "height":"96%",
+		        "margin-top": 110
+		    },
+			"series":formatDataSeries(dataSeries)
+		};
+		zingchart.render({
+		    id: divId,
+		    height: 400,
+		    width: '100%',
+		    data: chartData
+		});
+	};
 })(window.zingchart = window.zingchart || {});
